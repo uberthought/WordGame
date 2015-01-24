600a6382adbc4602d4f6c36feea97a773106b559
Philip Richardson <uberthought@gmail.com>
2015-01-24 10:07:11 -0500

    initial version

---
+++ app/src/main/java/com/example/philip/myapplication2/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.philip.myapplication2;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText)findViewById(R.id.editText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                StringBuilder sb = new StringBuilder();
+
+                boolean replace = false;
+                for(int i = 0; i < s.length(); i++)
+                {
+                    char ch = s.charAt(i);
+
+                    if (Character.isAlphabetic(ch))
+                        sb.append(ch);
+                    else
+                        replace = true;
+                }
+
+                if (replace)
+                    s.replace(0, s.length(), sb, 0, sb.length());
+
+                Log.d("addTextChangedListener", s.toString());
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
