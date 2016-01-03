eb6e6ee1acb620ea7d47fb2a47aaa825607414bc
Philip Richardson <uberthought@gmail.com>
2016-01-03 09:25:16 -0500

    added word rank to unabridged dictionary

--- app/src/main/java/com/example/philip/myapplication2/UnabridgedDictionary.java
+++ app/src/main/java/com/example/philip/myapplication2/UnabridgedDictionary.java
@@ -9,24 +9,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by philip on 11/28/15.
  */
 public class UnabridgedDictionary {
     private static UnabridgedDictionary instance;
-    ArrayList<String> wordList = new ArrayList<>();
+    HashMap<String, DictionaryWord> wordList;
 
     UnabridgedDictionary(Context context) {
         Resources resources = context.getResources();
-
 
         try {
             InputStream inputStream = resources.openRawResource(R.raw.google_10000_english);
             BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
             String line;
+            int rank = 0;
             while ((line = r.readLine()) != null) {
-                wordList.add(line);
+                wordList.put(line, new DictionaryWord(line, rank));
+                rank++;
             }
             inputStream.close();
         } catch (IOException exception) {
@@ -44,8 +46,9 @@ public class UnabridgedDictionary {
         return null;
     }
 
-    int GetRank(Word word) {
-        return -1;
+    int GetRank(String word) {
+        DictionaryWord dictionaryWord = wordList.get(word);
+        return dictionaryWord.Rank;
     }
 
     String GetPronounciation(Word word) {
